1d095672bfce6a40fd8d2cd77023137e8995e9bf
Eray Ozer <erayozer91@protonmail.com>
2021-01-11 00:58:09 +0100

    add caching mechanism

--- app/views.py
+++ app/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
+from django.core.cache import cache
 
 from .services import get_json_for_the_city
 from .forms import CityForm
+from .helpers import get_env_value
 
 def get_current_weather_in_city(request):
     if request.POST:
         form = CityForm(request.POST)
         city_name = form["name"].value()
-        result = get_json_for_the_city(city_name)
+        cache_key = city_name.lower()
+        cache_time = get_env_value("CACHING_TIME")
+        result = cache.get(city_name.lower())
+        if not result:
+            result = get_json_for_the_city(city_name)
+            cache.set(cache_key, result, int(cache_time))
         return render(request, "weather_forecast.html", {"result": result, "form": form})
     else:
         form = CityForm()
