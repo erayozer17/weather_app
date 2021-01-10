0b0dce6a67c921afcfe162507ebc4d8fcf8677a0
Eray Ozer <erayozer91@protonmail.com>
2021-01-10 23:17:08 +0100

    add weather_forecast interface

--- app/views.py
+++ app/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+from .services import get_weather_for_city
+
 def get_current_weather_in_city(request):
-    return render(request, 'deneme.html', {})
+    if request.POST:
+        city_name = request.POST["city_name"]
+        result = get_weather_for_city(city_name)
+        return render(request, 'weather_forecast.html', {"result": result})
