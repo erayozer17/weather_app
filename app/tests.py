22ecbc6909d8ba7ec4b10d8547dc5a6da7545679
Eray Ozer <erayozer91@protonmail.com>
2021-01-12 00:23:26 +0100

    add tests and improve error handling

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,54 @@
-# from django.test import TestCase
+import unittest
+from unittest import mock
 
-# Create your tests here.
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+from .forms import CityForm
+from .services import get_json_for_the_city
+
+
+class TestForm(unittest.TestCase):
+
+    def test_valid_form(self):
+        data = {'name': 'Cologne'}
+        form = CityForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_form(self):
+        data = {'name': ''}
+        form = CityForm(data=data)
+        self.assertFalse(form.is_valid())
+
+
+class TestView(SimpleTestCase):
+
+    def test_get_request_on_get_current_weather_in_city(self):
+        url = reverse("get_current_weather_in_city")
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'weather_forecast.html')
+        self.assertEqual(response.status_code, 200)
+        form = response.context['form']
+        self.assertIsInstance(form, CityForm)
+        self.assertIn('form', response.context)
+
+    def mock_get_json_for_the_city(*args, **kwargs):
+        return {"mocked": "mocked_value"}
+
+    @mock.patch("app.views.get_json_for_the_city", side_effect=mock_get_json_for_the_city)
+    def test_post_request_on_get_current_weather_in_city(self, mock_get):
+        url = reverse("get_current_weather_in_city")
+        response = self.client.post(url, data={"name": "Cologne"})
+        self.assertTemplateUsed(response, 'weather_forecast.html')
+        self.assertEqual(response.status_code, 200)
+        form = response.context['form']
+        self.assertIsInstance(form, CityForm)
+        self.assertIn('result', response.context)
+        self.assertIn('form', response.context)
+        self.assertEquals(response.context['result']['mocked'], 'mocked_value')
+
+    @mock.patch("app.views.get_json_for_the_city", side_effect=mock_get_json_for_the_city)
+    def test_post_invalid_request_on_get_current_weather_in_city(self, mock_get):
+        url = reverse("get_current_weather_in_city")
+        response = self.client.post(url, data={"name": ""})
+        self.assertEqual(response.status_code, 400)
--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.exceptions import SuspiciousOperation
 
 from .services import get_json_for_the_city
 from .forms import CityForm
@@ -7,6 +8,8 @@ from .forms import CityForm
 async def get_current_weather_in_city(request):
     if request.POST:
         form = CityForm(request.POST)
+        if(not form.is_valid()):
+            raise SuspiciousOperation("name field cannot be empty.")
         city_name = form["name"].value()
         result = await get_json_for_the_city(city_name)
         return render(request, "weather_forecast.html", {"result": result, "form": form})
@@ -16,16 +19,24 @@ async def get_current_weather_in_city(request):
 
 
 def custom_page_not_found_view(request, exception):
-    return render(request, "errors/404.html", {"message": exception})
+    response = render(request, "errors/404.html", {"message": exception})
+    response.status_code = 404
+    return response
 
 
 def custom_error_view(request, exception=None):
-    return render(request, "errors/500.html", {"message": exception})
+    response = render(request, "errors/500.html", {"message": exception})
+    response.status_code = 500
+    return response
 
 
 def custom_permission_denied_view(request, exception=None):
-    return render(request, "errors/403.html", {"message": exception})
+    response = render(request, "errors/403.html", {"message": exception})
+    response.status_code = 403
+    return response
 
 
 def custom_bad_request_view(request, exception=None):
-    return render(request, "errors/400.html", {"message": exception})
+    response = render(request, "errors/400.html", {"message": exception})
+    response.status_code = 400
+    return response
