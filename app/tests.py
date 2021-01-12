53d348334c61bb028b407442e3c266a13b81b5b5
Eray Ozer <erayozer91@protonmail.com>
2021-01-12 02:02:23 +0100

    add async test for service

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,4 @@
+import asyncio
 import unittest
 from unittest import mock
 
@@ -52,3 +53,53 @@ class TestView(SimpleTestCase):
         url = reverse("get_current_weather_in_city")
         response = self.client.post(url, data={"name": ""})
         self.assertEqual(response.status_code, 400)
+
+
+class TestService(unittest.TestCase):
+
+    expected_result_from_api = {
+        "weather": [
+            {
+                "description": "clear sky",
+            }
+        ],
+        "main": {
+            "temp": 282.55,
+            "temp_min": 280.37,
+            "temp_max": 284.26,
+            "pressure": 1023,
+            "humidity": 100
+        },
+        "wind": {
+            "speed": 1.5,
+            "deg": 350
+        },
+    }
+
+    def _run(self, coro):
+        return asyncio.get_event_loop().run_until_complete(coro)
+
+    def AsyncMock(*args, **kwargs):
+        m = mock.MagicMock(*args, **kwargs)
+
+        async def mock_coro(*args, **kwargs):
+            return m(*args, **kwargs)
+
+        mock_coro.mock = m
+        return mock_coro
+
+    @mock.patch("app.services.get_cache_or_call", new=AsyncMock(return_value=expected_result_from_api))
+    def test_get_json_for_the_city(self):
+        res = self._run(get_json_for_the_city("Cologne"))
+        expected = {
+            "city": "Cologne",
+            "temp": 282.55,
+            "temp_min": 280.37,
+            "temp_max": 284.26,
+            "pressure": 1023,
+            "humidity": 100,
+            "wind_speed": 1.5,
+            "wind_direction": "N",
+            "description": "Clear sky"
+        }
+        self.assertEqual(res, expected)
